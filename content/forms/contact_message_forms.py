53a3714e32c402843a36ebddbbbfe5b62a8caf6e
Marek Schir <schir2@msn.com>
2024-09-13 00:30:00 -0400

    added captcha

--- content/forms/contact_message_forms.py
+++ content/forms/contact_message_forms.py
@@ -1,9 +1,13 @@
 from django import forms
+from django_recaptcha.fields import ReCaptchaField
+from django_recaptcha.widgets import ReCaptchaV2Checkbox
 
 from content.models import ContactMessage
 
 
 class ContactMessageForm(forms.ModelForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
+
     class Meta:
         model = ContactMessage
         fields = ('name', 'email', 'subject', 'message')
