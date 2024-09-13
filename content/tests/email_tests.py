149114291ca32b9d62aa18bb1b88df1ffb4e39fe
Marek Schir <schir2@gmail.com>
2024-09-13 17:25:53 -0400

    setup smtp email backend

---
+++ content/tests/email_tests.py
@@ -0,0 +1,17 @@
+from django.core.mail import EmailMessage
+from django.test import TestCase
+from django.test.utils import override_settings
+
+from schirDev import settings
+
+
+@override_settings(EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend')
+class EmailTests(TestCase):
+    subject = 'Test Email'
+    message = 'This is a test email sent using SMTP in Django <3.'
+    from_email = settings.EMAIL_HOST_USER
+    recipient_list = ['schir2@gmail.com']
+
+    def test_send_mail(self):
+        email = EmailMessage(self.subject, self.message, self.from_email, self.recipient_list)
+        email.send(False)
