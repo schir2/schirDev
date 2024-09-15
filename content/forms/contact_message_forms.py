4eb549a8f23acec620242cd452183d8d0bf5ec8d
Marek Schir <schir2@gmail.com>
2024-09-15 15:31:32 -0400

    remoted dark theming

--- content/forms/contact_message_forms.py
+++ content/forms/contact_message_forms.py
@@ -6,7 +6,37 @@ from content.models import ContactMessage
 
 
 class ContactMessageForm(forms.ModelForm):
-    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
+    SUBJECT_CHOICES = [
+        ('contract', 'Contract Work'),
+        ('full_time', 'Full-Time Opportunity'),
+        ('general_inquiry', 'General Inquiry'),
+    ]
+
+    name = forms.CharField(
+        label="Full Name",
+        help_text="Please enter your full name or the name of your organization.",
+        widget=forms.TextInput(attrs={'placeholder': 'Full Name or Organization Name'})
+    )
+    email = forms.EmailField(
+        label="Email Address",
+        help_text="Enter your business or contact email address. We will use this to follow up.",
+        widget=forms.EmailInput(attrs={'placeholder': 'Your Email Address'})
+    )
+    subject = forms.ChoiceField(
+        label="Reason for Contact",
+        help_text="Select the type of inquiry.",
+        choices=SUBJECT_CHOICES,
+        widget=forms.Select(attrs={'type': 'select'})
+    )
+    message = forms.CharField(
+        label="Message",
+        help_text="Please provide details about the opportunity, project, or your inquiry. Feel free to include any relevant information or questions.",
+        widget=forms.Textarea(attrs={'placeholder': 'Your Message'})
+    )
+    captcha = ReCaptchaField(
+        widget=ReCaptchaV2Checkbox,
+        help_text="Prove you're not a robot."
+    )
 
     class Meta:
         model = ContactMessage
