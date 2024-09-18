17761c0a2458c1ed563ae0fd59beb36ba2921f50
Marek Schir <schir2@msn.com>
2024-09-17 21:13:41 -0400

    added OverwriteStorage storage

---
+++ content/storages.py
@@ -0,0 +1,12 @@
+# content/storages.py
+
+import os
+
+from django.core.files.storage import FileSystemStorage
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        if self.exists(name):
+            os.remove(os.path.join(self.location, name))
+        return name
