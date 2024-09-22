0a83b6a3a14b2d921a2ec2d66648d5acf2593217
Marek Schir <schir2@gmail.com>
2024-09-22 14:06:42 -0400

    added article view functions

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from blog.models import Article
+
+
+def home_view(request):
+    context = {}
+    template_name = ''
+    return render(request, template_name=template_name, context=context)
+
+
+def article_list_view(request):
+    template_name = 'blog/article_list.html'
+    context = {}
+    article_list = Article.objects.all()
+    context['article_list'] = article_list
+    return render(request, template_name=template_name, context=context)
+
+
+def article_detail_view(request, slug):
+    template_name = 'blog/article.html'
+    context = {}
+    article = Article.objects.get(slug=slug)
+    context["article"] = article
+    return render(request, template_name=template_name, context=context)
