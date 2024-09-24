5fb2c9f9b55b297fc16bc711f273047018ad1cf7
Marek Schir <schir2@gmail.com>
2024-09-23 23:12:07 -0400

    added articles to blog_index_view

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from blog.models import Article
 def blog_index_view(request):
     context = {}
     template_name = 'blog/index.html'
+    articles = Article.objects.all()
+    context['articles'] = articles
     return render(request, template_name=template_name, context=context)
 
 
