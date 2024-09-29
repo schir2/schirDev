b0a00320a487492eaa2feb6afb040f65d64fbeb2
Marek Schir <schir2@gmail.com>
2024-09-29 12:20:21 -0400

    added custom logging

---
+++ core/extensions/logging.py
@@ -0,0 +1,24 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'root': {
+        'handlers': ['console', 'file'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'blog': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
