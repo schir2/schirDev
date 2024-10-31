f3d930c3615c7650b8746e3ec5db8733d16a6d7c
Marek Schir <schir2@gmail.com>
2024-10-30 21:42:55 -0400

    moved email settings to messaging extension

---
+++ core/extensions/messaging.py
@@ -0,0 +1,9 @@
+from core.settings import env
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
