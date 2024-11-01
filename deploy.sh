b4838989188ee89367ebb1f881431144deac53ba
Marek Schir <schir2@gmail.com>
2024-11-01 00:37:02 -0400

    added ability to skip tailwind and static building in deploy command

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,20 @@ VENV_DIR="$PROJECT_DIR/venv"
 GUNICORN_SERVICE="gunicorn"
 NGINX_SERVICE="nginx"
 
+# Parse arguments
+SKIP_TAILWIND=false
+SKIP_STATIC=false
+for arg in "$@"; do
+  case $arg in
+    --skip-tailwind)
+      SKIP_TAILWIND=true
+      ;;
+    --skip-static)
+      SKIP_STATIC=true
+      ;;
+  esac
+done
+
 echo "Starting deployment script..."
 
 # Step 1: Pull the latest code from GitHub
@@ -18,12 +32,20 @@ echo "Activating virtual environment and installing dependencies..."
 source $VENV_DIR/bin/activate
 pip install -r requirements.txt
 
-# Step 3: Collect static files
-echo "Building Tailwind"
-python manage.py tailwind build
+# Step 3: Build Tailwind and collect static files
+if [ "$SKIP_TAILWIND" = false ]; then
+  echo "Building Tailwind"
+  python manage.py tailwind build
+else
+  echo "Skipping Tailwind build as per --skip-tailwind flag"
+fi
 
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
+if [ "$SKIP_STATIC" = false ]; then
+  echo "Collecting static files..."
+  python manage.py collectstatic --noinput
+else
+  echo "Skipping static files collection as per --skip-static flag"
+fi
 
 # Step 4: Run database migrations
 echo "Running database migrations..."
