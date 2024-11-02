d5b32e69b89476376d8d47aecef0109b51edcb7d
Marek Schir <schir2@gmail.com>
2024-11-02 15:34:00 -0400

    yay

--- tools/static/tools/js/retirement_calculator/pipelines.js
+++ tools/static/tools/js/retirement_calculator/pipelines.js
@@ -1,4 +1,49 @@
-const pipelines = {
+const process = {
+
+    ElectiveLimitsProcess: {
+        adjustCatchUpLimit(row) {
+            row.taxDeferredContributionCatchUpLimit = 7500
+            return row;
+        },
+        adjustElectiveLimit(row) {
+            if (row.taxDeferredContributionElectiveLimit === undefined) {
+                row.taxDeferredContributionElectiveLimit = 22500
+            } else {
+                row.taxDeferredContributionElectiveLimit = row.taxDeferredContributionElectiveLimit * (1 + row.taxDeferredContributionElectiveLimitInflationRate / 100);
+            }
+            return row
+        },
+        adjustElectiveTotalLimit(row) {
+            if (row.taxDeferredContributionTotalElectiveLimit === undefined) {
+                row.taxDeferredContributionTotalElectiveLimit = 66000
+            } else {
+
+                row.taxDeferredContributionTotalElectiveLimit = row.taxDeferredContributionTotalElectiveLimit * (1 + row.taxDeferredContributionElectiveLimitInflationRate / 100);
+            }
+            return row
+        },
+        calculateAgeAdjustedLimit(row) {
+            row.taxDeferredContributionElectiveLimitApplied = row.age < 50 ? row.taxDeferredContributionElectiveLimit : row.taxDeferredContributionElectiveLimit + row.taxDeferredContributionCatchUpLimit
+            console.log(row.taxDeferredContributionElectiveLimitApplied)
+            return row
+        },
+        calculateAgeAdjustedTotalLimit(row) {
+            row.taxDeferredContributionElectiveTotalLimitApplied = row.age < 50 ? row.taxDeferredContributionTotalElectiveLimit : row.taxDeferredContributionTotalElectiveLimit + row.taxDeferredContributionCatchUpLimit
+            return row
+        },
+        process(row) {
+            row = this.adjustCatchUpLimit(row)
+            row = this.adjustElectiveLimit(row)
+            row = this.adjustElectiveTotalLimit(row)
+            row = this.calculateAgeAdjustedLimit(row)
+            row = this.calculateAgeAdjustedTotalLimit(row)
+            return row
+        },
+    }
+
+}
+
+const pipeline = {
     CompanyMatchPipeline: {
         process(row) {
             row.taxDeferredContributionElectiveLimitApplied = this.getTaxDeferredContributionElectiveLimitApplied(row);
