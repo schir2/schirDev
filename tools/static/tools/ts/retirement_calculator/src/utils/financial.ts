c3d6978e98138e50db935f218f492ad36e35de1d
Marek Schir <schir2@gmail.com>
2024-11-09 13:26:27 -0500

    added financial utils with compound interest

---
+++ tools/static/tools/ts/retirement_calculator/src/utils/financial.ts
@@ -0,0 +1,3 @@
+export function calculateCompoundInterest(principal: number, interestRate: number, numberOfInterestApplicationsPerPeriod: number = 1, numberOfPeriods: number): number {
+    return principal * (1 + (interestRate / numberOfInterestApplicationsPerPeriod)) ** (numberOfInterestApplicationsPerPeriod * numberOfPeriods)
+}
